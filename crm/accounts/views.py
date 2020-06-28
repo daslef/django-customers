f3f4e38eb37706cd6c3ceb03b799dc96e048be80
daslef <daslef93@gmail.com>
2020-06-28 19:54:50 +0300

    dashboard dynamic template

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,13 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    orders = Order.objects.all()
+    total_orders = orders.count()
+    orders_pending = orders.filter(status='Pending').count()
+    orders_delivered = orders.filter(status='Delivered').count()
+    context = {
+        'orders': orders, 
+        'customers': customers, 
+        'orders_total': total_orders,
+        'orders_pending': orders_pending,
+        'orders_delivered': orders_delivered
+        }
+    
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
