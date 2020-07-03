84cd3fc005e709e9bca0041bc9a4b84f1e0ef64e
daslef <daslef93@gmail.com>
2020-07-03 14:09:58 +0300

    reset-password over email

--- crm/accounts/urls.py
+++ crm/accounts/urls.py
@@ -1,17 +1,44 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
     path('signup/', views.signup_handle, name='signup'),
     path('login/', views.login_handle, name='login'),
     path('logout/', views.logout_handle, name='logout'),
+    
     path('user/', views.userProfile, name='user-profile'),
     path('settings/', views.userSettings, name='user-settings'),
     path('products/', views.products, name='products'),
     path('customer/<str:pk>/', views.customer, name='customer'),
+    
     path('create_order/<str:pk>', views.createOrder, name='create_order'),
     path('update_order/<str:pk>', views.updateOrder, name='update_order'),
     path('delete_order/<str:pk>', views.deleteOrder, name='delete_order'),
+    
+    path(
+        'reset_password', 
+        auth_views.PasswordResetView.as_view(),
+        name='reset_password'
+        ),
+
+    path(
+        'reset_password_sent', 
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+
+    path(
+        'reset_password/<uidb64>/<token>', 
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+
+    path(
+        'reset_password_complete', 
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'
+        )
+
 ]
--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from .email import EmailConfig
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,14 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+# SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'daslef93@gmail.com'
+EMAIL_HOST_PASSWORD = EmailConfig.DJANGO_EMAIL_HOST_PASSWORD
+print(EMAIL_HOST_PASSWORD)
